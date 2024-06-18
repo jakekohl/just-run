b38781b42aa88b0edb624a9db0b2ff97e0f012c6
Jake Kohl <jacob.jp.kohl@gmail.com>
2024-06-18 00:15:31 -0500

    Chapter 1 of building express server

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 let express = require('./src/express')
 const app = express()
 
-app.get('/', (req, res,next) => {
-    console.log(next)
-    next()
-});
-
-
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/website/dashboard.html');
-});
-
-app.post('/post',(req,res) => {
     res.writeHead(200)
-    res.write('Data from post :)');
+    res.write('Hello world!');
     res.end();
-})
+});
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
