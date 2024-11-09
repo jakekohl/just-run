b373aaffba2735e599ad89347dcb1c4658983ce7
Jake Kohl <jacob.jp.kohl@gmail.com>
2024-11-09 16:10:02 -0600

    base url definition

--- cypress/e2e/smoke/smoke.cy.js
+++ cypress/e2e/smoke/smoke.cy.js
@@ -1,3 +1,5 @@
+const baseUrl = Cypress.config('e2e').baseUrl
+
 describe('Smoke Tests for PR requests', () => {
   it('should load the page', () => {
     cy.visit(baseUrl).its('status').should('eq', 200)
