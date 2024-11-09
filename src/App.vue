63cb7780f2cd824bad8e19f7840a56d7a920fac0
Jake Kohl <jacob.jp.kohl@gmail.com>
2024-11-09 15:20:22 -0600

    It's a start I guess

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,100 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <nav class="top-nav">
+    <div class="left-nav">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="right-nav">
+      <button @click="toggleSubMenu">Menu</button>
+      <div v-if="showSubMenu" class="sub-menu">
+        <button @click="openLoginModal">Login</button>
+        <RouterLink to="/Account">Account</RouterLink>
+      </div>
+    </div>
+  </nav>
+  <div class="content">
+    <RouterView />
+  </div>
+  <LoginModal v-if="showLoginModal" @close="closeLoginModal" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+import LoginModal from '@/components/LoginModal.vue'
+
+export default {
+  data() {
+    return {
+      showSubMenu: false,
+      showLoginModal: false,
+    }
+  },
+  methods: {
+    toggleSubMenu() {
+      this.showSubMenu = !this.showSubMenu
+    },
+    openLoginModal() {
+      this.showLoginModal = true
+    },
+    closeLoginModal() {
+      this.showLoginModal = false
+    },
+  },
+  components: {
+    LoginModal,
+  },
+}
+</script>
+
+<style>
+.top-nav {
+  display: flex;
+  align-items: center;
+  background-color: #272626;
+  padding: 10px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.left-nav {
+  display: flex;
+  align-items: center;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.left-nav RouterLink {
+  margin-right: 50px; /* Add spacing between the links */
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.right-nav {
+  margin-left: auto; /* Push the right-nav to the right side */
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.right-nav button {
+  background-color: #dad84f;
+  padding: 10px;
+  border: none;
+  cursor: pointer;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.right-nav RouterLink {
+  margin-left: 50px; /* Add spacing between the links */
 }
 
-nav a:first-of-type {
-  border: 0;
+.content {
+  margin-top: 20px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.sub-menu {
+  position: absolute;
+  top: calc(100% + 10px);
+  right: 0;
+  margin-left: 10px;
+  background-color: #dad84f;
+  padding: 10px;
+  border: 1px solid #050505;
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Welcome to my Vue page!</h1>
+    <p>This is a simple Vue page created using GitHub Copilot.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+
+p {
+  font-size: 18px;
+}
+</style>
