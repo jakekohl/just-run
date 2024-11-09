0a59e7e500521bef9641cd5bb3a4f550587582b5
Jake Kohl <jacob.jp.kohl@gmail.com>
2024-11-09 15:48:34 -0600

    placeholders....

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>About Page</h1>
+    <p>Welcome to our app! This is the About page.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
+
+<style scoped>
+.about {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
---
+++ src/views/Account.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Account Settings</h1>
+    <form>
+      <div>
+        <label for="name">Name:</label>
+        <input type="text" id="name" v-model="name" />
+      </div>
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email" />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <button @click="saveSettings">Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    saveSettings() {
+      // TODO: Implement save settings logic
+    },
+  },
+}
+</script>
+
+<style scoped>
+/* Add your custom styles here */
+</style>
